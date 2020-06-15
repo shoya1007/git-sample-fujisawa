72e710e5c5fc4ab156caeba7d0109891e6c86ed8
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2020-06-15 14:29:09 +0900

    新規にプロジェクトを作成

---
+++ src/main/java/com/example/GitSample2FujisawaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSample2FujisawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSample2FujisawaApplication.class, args);
+	}
+
+}
